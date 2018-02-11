d44803410826f4209c4e3a84d446a2e2d2d1ae09
Jaime <jaimebmorin@gmail.com>
2018-02-11 13:51:00 -0600

    part 1 finished; I will return for optional parts(2 and 3)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+#To whoever is grading this - I understand this program was not meant to be run on your local machine but I HIGHLY recommend you follow the instructions and run the program on your local machine(I've made it very easy) to look at my data validations(where I spent a good chunk of time)for the inquirer package and also to see how I'm starting to deal problems stemmed from asyncronous programming languages#
+
 #Welcome to my Bamazon Homework Assignment#
 
 ##Steps to run program on local computer##
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,9 @@
+//goal 1 - display all items for sale.
+//goal 2 - inquirer customer responses of what they want to buy.
+//goal 3 - ensure the item is in stock.
+//goal 4 - update MYSQL database after sale purchase.
+
+
 //node packages required for this program
 
 var mysql = require ("mysql");
@@ -12,6 +18,7 @@ var connection = mysql.createConnection({
   });
 
 
+
 //connect to MYSQL and console out status
 connection.connect(function(err) {
     if (err) {
@@ -24,10 +31,8 @@ connection.connect(function(err) {
 //goal 1 - display all items for sale.
 //I've decided to put my inquirer inside of this portion of code in order to validate the answers - I'm not certain how else to do it.
 //I will research better options as this seems sort of sloppy to me.
-//goal 2 - inquirer customer responses of what they want to buy.
-//goal 3 - ensure the item is in stock.
-//goal 4 - update MYSQL database.
 
+function runMain() {
 connection.query("SELECT * FROM products ORDER BY id", function(err, results) {
     //goal 1:
     if (err) {
@@ -39,15 +44,19 @@ connection.query("SELECT * FROM products ORDER BY id", function(err, results) {
         }
     }
     //goal 2:
-    //First time writing my own data validation for the inquirer package:
+    //First time writing my own data validation for the inquirer package
+    //everything seems to be working :-) I've found a few ways to "break" the validation but for now this is good enough.
+    //I will reasearch what is going on at a later date. I've found for the second question if I enter an integer followed
+    //by a number(for example 2344E) it'll accept the answer; but for most other cases it works.
     var questions = [
         {
             type: 'input',
             name: 'itemWanted',
             message: 'What item would you like to buy?',
             validate: function(idCheck) {
+                parseInt(idCheck);
                 for (var i = 0; i < results.length; i++){
-                    if (idCheck === results[i].id) {return true}  
+                    if (idCheck == results[i].id) {return true}  
                 }
                 return 'Please enter a valid product id';  
             }
@@ -63,14 +72,53 @@ connection.query("SELECT * FROM products ORDER BY id", function(err, results) {
         }
     ]
 
+    //use inquirer to ask the questions and receive the answers
+    
     inquirer.prompt(questions).then(answers => {
-        console.log('\nOrder request:');
-        console.log(JSON.stringify(answers, null, '  '));
-      });
+    var itemNeeded = parseInt(answers.itemWanted) - 1;
+    var quantNeeded = parseInt(answers.quantity);
+    if (answers.quantity == 1) {
+        console.log("You've ordered " + quantNeeded + " " + results[itemNeeded].product_name);
+    } else {
+        console.log("You've ordered " + quantNeeded + " " + results[itemNeeded].product_name + "s");
+    }
+    
+    //goal 3:
+
+    if (quantNeeded > results[itemNeeded].stock_quantity){
+        console.log("We do not have that many units in stock; we only have " + results[itemNeeded].stock_quantity + " -Please try again")
+        runMain();
+    }
+
+    var price = results[itemNeeded].price * quantNeeded;
+    console.log("The total purchase price for your order is $" + price );
+
+    //goal 4
+    var remainder = results[itemNeeded].stock_quantity - quantNeeded;
+    var idNeeded = results[itemNeeded].id;
+    var sql = 'UPDATE products SET stock_quantity = ? WHERE id = ?'
+
+    connection.query(sql, [remainder, idNeeded], function(err, results2) {
+        console.log("our records have been updated and your order has been proccessed - thanks!")
+    });
+
+    setTimeout(function() {
+    var questions2 = [
+        {
+            type: 'confirm',
+            name: 'buyAgain',
+            message: 'would you like to purchase another item?',
+            default: false
+        }
+    ]
+    inquirer.prompt(questions2).then(answers2 => {
+        if (answers2.buyAgain) {runMain()}
+        else {connection.end();};
+    });
+    }, 500);
+    });
 });
+}
 
 
-
-
-
-
+runMain();
